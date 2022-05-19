1f741e12f7b482130b370be5e97b8ac492b3a594
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-05-19 13:56:52 -0300

    welckson valentim - feat: List, desenvolve lógica para renderizar lista de tarefas

---
+++ src/Pages/List.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const API_ENDPOINT = 'https://listofcarsbackend.herokuapp.com/cars';
+
+function List() {
+  const [ loading, setLoading ] = useState(true);
+  const [ data ] = useState([]);
+
+  useEffect(() => {
+    const search = () => {
+    const result =  axios.get(API_ENDPOINT)
+        .then(response => response.data);
+    return result;
+    }
+
+    async function getList() {
+      try {
+        const list = await search();
+        return list;
+      } catch (err) {
+        console.log(err);
+        return [];
+      }
+    }
+
+    getList().then(async (fcs) => {
+    data.push(fcs);
+    if(fcs.message) return alert('Task already exist')
+    setLoading(false)
+    }, [data]);
+  })
+
+  if (loading) return <h1>loading...</h1>;
+  return (
+    <main className='tasks'>
+      {data[0].map((task) => 
+        <section className='sectiontasks'>
+          <p>{`Description: ${task.description}`}</p>
+          <h4>{`Status: ${task.status}`}</h4>
+          <h4>{`Date: ${task.date}`}</h4>
+          <h4>{`Priority: ${task.priority}`}</h4>
+        </section>
+      )}
+    </main>
+  );
+}
+
+export default List;
