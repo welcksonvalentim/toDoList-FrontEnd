36bc4814a98754fb4e17ebfbd80902053778e23a
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-05-19 16:09:41 -0300

    welckson valentim - build: AdTask, cria componente para adição de novas tarefas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Components/Header';
-import List from './Pages/List';
 import Footer from './Components/Footer';
+import List from './Pages/List';
+import AdTask from './Pages/AdTask';
 import './App.css';
 
 function App() {
   return (
     <section className='links-header'>
       <Header />
+      <AdTask />
       <List />
       <Footer />
     </section>
---
+++ src/Pages/AdTask.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdTask() {
+  return (
+    <main className='tasks'>
+      <h2>Adiciona Tarefa</h2>
+    </main>
+  );
+}
+
+export default AdTask;
